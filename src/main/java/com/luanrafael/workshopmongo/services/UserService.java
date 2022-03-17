0633b36628bf6392008a31c76a06195a7eb3b72b
Luan Rafael <luanraffaell@hotmail.com>
2022-03-17 04:05:17 -0300

    Inserção de usuário com POST

--- src/main/java/com/luanrafael/workshopmongo/services/UserService.java
+++ src/main/java/com/luanrafael/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luanrafael.workshopmongo.domain.User;
+import com.luanrafael.workshopmongo.dto.UserDto;
 import com.luanrafael.workshopmongo.repository.UserRepository;
 import com.luanrafael.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,12 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
