e593a98d3d2973770b61f58f249c8d3df43cb681
Luan Rafael <luanraffaell@hotmail.com>
2022-03-18 01:27:48 -0300

    Consulta simples com query methods

--- src/main/java/com/luanrafael/workshopmongo/resources/PostResource.java
+++ src/main/java/com/luanrafael/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.luanrafael.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luanrafael.workshopmongo.domain.Post;
+import com.luanrafael.workshopmongo.resources.util.URL;
 import com.luanrafael.workshopmongo.services.PostService;
 
 @RestController
@@ -22,6 +26,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	//http://localhost:8080/posts/titlesearch?text=Bom%20dia
+	@RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- src/main/java/com/luanrafael/workshopmongo/services/PostService.java
+++ src/main/java/com/luanrafael/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.luanrafael.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String txt){
+		return repo.findByTitleContainingIgnoreCase(txt);
+	}
 }
