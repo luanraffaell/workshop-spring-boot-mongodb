e70163388c0989a32f52f4913a7088954dcdecbc
Luan Rafael <luanraffaell@hotmail.com>
2022-03-17 23:41:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/luanrafael/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luanrafael/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.luanrafael.workshopmongo.domain.Post;
 import com.luanrafael.workshopmongo.domain.User;
+import com.luanrafael.workshopmongo.dto.AutorDTO;
 import com.luanrafael.workshopmongo.repository.PostRepository;
 import com.luanrafael.workshopmongo.repository.UserRepository;
 
@@ -18,30 +19,30 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
-		userRepository.deleteAll(); //para limpar nossa coleção no mongodb
+
+		userRepository.deleteAll(); // para limpar nossa coleção no mongodb
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		User cibele = new User(null, "Cibele purple", "cibele@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",maria);
-		
-		
-		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob,cibele));
-		postRepository.saveAll(Arrays.asList(post1,post2));
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, cibele));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços!",
+				new AutorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AutorDTO(maria));
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
--- src/main/java/com/luanrafael/workshopmongo/domain/Post.java
+++ src/main/java/com/luanrafael/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.luanrafael.workshopmongo.dto.AutorDTO;
+
 @Document //para indicar que é um documento mongo
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -16,12 +18,12 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User autor;
+	private AutorDTO autor;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body,User autor) {
+	public Post(String id, Date date, String title, String body,AutorDTO autor) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -61,11 +63,11 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAutor() {
+	public AutorDTO getAutor() {
 		return autor;
 	}
 
-	public void setAutor(User autor) {
+	public void setAutor(AutorDTO autor) {
 		this.autor = autor;
 	}
 	
